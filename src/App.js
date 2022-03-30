b497ad0182a488dff26ab732f47aead4354db8f6
Eliyas Hossain <eliyashossain700@gmail.com>
2022-03-31 00:37:38 +0600

    Coin Detail Showed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About/About";
+import CoinDetails from "./Components/CoinDetails/CoinDetails";
 import Coins from "./Components/Coins/Coins";
 import BdOffice from "./Components/Contact/BdOffice";
 import Contact from "./Components/Contact/Contact";
@@ -15,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/coins" element={<Coins />} />
+        <Route
+          path="/coin-details/:id"
+          element={<CoinDetails></CoinDetails>}
+        ></Route>
         <Route path="/contact" element={<Contact />}>
           <Route path="us-office" element={<UsOffice />}></Route>
           <Route path="bd-office" element={<BdOffice />}></Route>
--- src/Components/CoinCard/CoinCard.js
+++ src/Components/CoinCard/CoinCard.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CoinCard = ({ coin }) => {
   console.log(coin);
   const { image, name, symbol } = coin;
   return (
-    <div className="flex justify-between items-center shadow-lg rounded-2xl w-[250px] bg-white p-4">
-      <img
-        className="object-cover rounded-full h-16 w-16 "
-        src={image}
-        alt=""
-      />
-      <div>
-        <h5 className="text-gray-600 font-medium">{name}</h5>
-        <p className="text-gray-400 text-xs">{symbol}</p>
-      </div>
+    <div>
+      <Link to={`/coin-details/${coin.id}`}>
+        <div className="flex justify-between items-center shadow-lg rounded-2xl w-[250px] bg-white p-4">
+          <img
+            className="object-cover rounded-full h-16 w-16 "
+            src={image}
+            alt=""
+          />
+          <div>
+            <h5 className="text-gray-600 font-medium">{name}</h5>
+            <p className="text-gray-400 text-xs">{symbol}</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
